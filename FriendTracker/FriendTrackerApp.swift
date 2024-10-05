bb9dd39bb0b1737f185adb8346f34589695717d3
Clint Bush <clint@madebyclint.com>
2024-10-05 13:28:49 -0700

    Initial Commit

---
+++ FriendTracker/FriendTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendTrackerApp.swift
+//  FriendTracker
+//
+//  Created by Clint Bush on 10/5/24.
+//
+
+import SwiftUI
+
+@main
+struct FriendTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
