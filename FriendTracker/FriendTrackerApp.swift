7045610f2c8d52753f8d07ce4a01fcc1853e3347
Clint Bush <clint@madebyclint.com>
2024-10-05 13:31:20 -0700

    Initial commit of basic app from Birthdays Apple Tutorial for Swift

--- FriendTracker/FriendTrackerApp.swift
+++ FriendTracker/FriendTrackerApp.swift
@@ -12,6 +12,7 @@ struct FriendTrackerApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .modelContainer(for: Friend.self)
         }
     }
 }
---
+++ FriendTracker/Models/Friend.swift
@@ -0,0 +1,25 @@
+//
+//  Friend.swift
+//  MyFriendTracker
+//
+//  Created by Clint Bush on 10/5/24.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+class Friend {
+    var name: String
+    var birthday: Date
+    
+    init(name: String, birthday: Date) {
+        self.name = name
+        self.birthday = birthday
+    }
+    
+    var isBirthdayToday: Bool {
+        Calendar.current.isDateInToday(birthday)
+    }
+}
+
